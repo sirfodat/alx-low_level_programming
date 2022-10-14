0e28162d597190a1f46367f19ba245867fadc4f5
Funmilayo Olawole <funmilayoolawole12gmail.com>
2022-10-13 18:59:03 -0700

    my 5-printf.c task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main -entry point for the program
  *
- * main - Entry point
- *
- * Return: Always 0 (Success)
- *
+ * Return:0 if no errors, return non zero if errors
  */
-
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
