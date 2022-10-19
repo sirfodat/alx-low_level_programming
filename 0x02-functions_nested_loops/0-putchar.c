e5242a14a49f675ad34dffe0190f006a6ee18aa0
Funmilayo Olawole <funmilayoolawole12gmail.com>
2022-10-19 11:44:05 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
+
 /**
- * main - Entry point
- *
+ * main - Print _putchar
  * Return: Always 0
- *
  */
 int main(void)
 {
-	_putchar(");
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
